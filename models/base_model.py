04e2a058bc8c0297476771f3fc4f8056faa265be
James Kariuki <karisjaymo99@gmail.com>
2023-05-13 13:04:51 +0300

    create file-storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 '''Base model'''
 
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -9,22 +10,27 @@ class BaseModel():
     '''defines all common attributes/methods for other classes'''
 
     def __init__(self, *args, **kwargs):
-        if kwargs:
+        if kwargs and len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if getattr(self, "created_at", None) and isinstance(self.created_at, str):
-                self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            if getattr(self, "created_at", None)\
+                    and isinstance(self.created_at, str):
+                self.created_at = datetime.strptime(self.created_at,
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
             else:
-                self.created_at = datetime.today()
-            if getattr(self, "updated_at", None) and isinstance(self.updated_at, str):
-                self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+                self.created_at = datetime.utcnow()
+            if getattr(self, "updated_at", None)\
+                    and isinstance(self.updated_at, str):
+                self.updated_at = datetime.strptime(self.updated_at,
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
             else:
-                self.updated_at = datetime.today()
+                self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -33,7 +39,8 @@ class BaseModel():
         '''updates the public instance attribute "updated_at"
         with the current datetime
         '''
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.utcnow().isoformat()
+        storage.save()
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values of "__dict__"
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''File Storage module'''
+
+import json
+
+
+class FileStorage():
+    '''serializes instances to a JSON file and
+    deserializes JSON file to instances
+    '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''returns a dictionary containing all objects
+        '''
+        return self.__objects
+
+    def new(self, obj):
+        '''sets/adds an object to a dictionary
+        '''
+        obj_classname = str(obj.__class__.__name__)
+        obj_id = getattr(obj, "id", None)
+
+        if obj_id:
+            new_key = obj_classname + '.' + obj_id
+            self.__objects[new_key] = obj.to_dict()
+
+    def save(self):
+        '''serializes all objects to the JSON file in __file_path
+        '''
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            json.dump(self.__objects, f, indent=4)
+
+    def reload(self):
+        '''deserializes the JSON file in __file_path to __objects
+        '''
+        try:
+            with open(self.__file_path, "r", encoding='utf-8') as f:
+                self.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
