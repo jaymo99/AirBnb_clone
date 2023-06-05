4c5b2fe1dabd25c5644214128da69a28e6feb1c4
James Kariuki <karisjaymo99@gmail.com>
2023-06-05 20:52:46 +0300

    define 'update' command for hbnb cmd

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AirBnb_clone
-![hbnb](graphics/hbnb.png)
+![hbnb](web_static/images/hbnb.png)
 
 ## Overview
 This is an open-source AirBnb clone build with Python (Flask). The project allows users to browse and book accomodation listings, manage their reservations, and leave reviews for their experiences. The application has been designed to provide a simpler but similar user experience to the official [AirBnb website](https://www.airbnb.com/), while also allowing for customization and additional features.
@@ -9,5 +9,15 @@ The project is still in its initial stages. The end goal is to have a complete w
 * A website (the front-end) that shows the final product to everybody: static and dynamic
 * A database or files that store data (data = objects)
 * An API that provides a communication interface between the front-end and the data (retrieve, create, delete, update them)
+
+## Quickstart
+1. Clone this repo
+```git
+git clone git@github.com:jaymo99/AirBnb_clone.git
+```
+2. Initialize all git submodules in this repo
+```git
+git submodule update --init --recursive
+```
 
 ## 1. The command interpreter
--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,8 +18,22 @@ class HBNBCommand(cmd.Cmd):
             "cls_missing": "** class name missing **",
             "cls_unexist": "** class doesn't exist**",
             "id_missing": "** instance id missing **",
-            "id_unexist": "** no instance found **"
+            "id_unexist": "** no instance found **",
+            "attr_missing": "** attribute name missing **",
+            "val_missing": "** value missing **"
             }
+
+    @classmethod
+    def __cast_attribute(self, value):
+        '''casts a string value to appropriate data type
+        '''
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return str(value)
 
     def emptyline(self):
         '''ignore empty lines'''
@@ -34,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         '''creates a new instance of <BaseModel> and
-        prints the id
+        prints the id.
         '''
         if not line:
             print(HBNBCommand.__err_msgs["cls_missing"])
@@ -48,11 +63,23 @@ class HBNBCommand(cmd.Cmd):
             new_obj.save()
             print(new_obj.id)
 
+    def help_create(self):
+        '''displays information about the `create` command
+        '''
+        print("\nUsage: create <class-name>\n"
+              "-> Creates a new instance of a class/model\n"
+              "   and displays its id\n"
+              "\nDocumented class-names:\n"
+              "========================")
+        for cls in HBNBCommand.__classes:
+            print(cls)
+        print()
+
     def do_show(self, line):
         '''prints the string representation of an instance
-        based on the class name and id
+        based on the class name and id.
         '''
-        obj_dict = storage.all()
+        all_objs = storage.all()
         if not line:
             print(HBNBCommand.__err_msgs["cls_missing"])
             return
@@ -62,21 +89,33 @@ class HBNBCommand(cmd.Cmd):
             print(HBNBCommand.__err_msgs["cls_unexist"])
         elif len(line) < 2:
             print(HBNBCommand.__err_msgs["id_missing"])
-        elif f"{line[0]}.{line[1]}" not in obj_dict:
+        elif f"{line[0]}.{line[1]}" not in all_objs:
             print(HBNBCommand.__err_msgs["id_unexist"])
         else:
-            obj_str = obj_dict["{}.{}".format(line[0], line[1])]
-            new_obj = HBNBCommand.__classes[line[0]](obj_str)
-            print(new_obj)
+            obj_key = "{}.{}".format(line[0], line[1])
+            obj = all_objs[obj_key]
+            print(obj)
+
+    def help_show(self):
+        '''displays information about the `show` command
+        '''
+        print("\nUsage: show <class-name> <id>\n"
+              "-> prints the string representation of an instance\n"
+              "   based on the class-name and id\n"
+              "\nDocumented class-names:\n"
+              "========================")
+        for cls in HBNBCommand.__classes:
+            print(cls)
+        print()
 
     def do_all(self, line):
         '''prints the string representation of all instances
         based or not on the class name.
         '''
-        all_dict = storage.all()
+        all_objs = storage.all()
 
         if not line:
-            print([str(obj) for obj in all_dict.values()])
+            print([str(obj) for obj in all_objs.values()])
             return
 
         line = line.split()
@@ -84,13 +123,83 @@ class HBNBCommand(cmd.Cmd):
             print(HBNBCommand.__err_msgs["cls_unexist"])
         else:
             cls = line[0]
-            print([str(obj) for obj in all_dict.values()\
-                    if obj.__class__.__name__ == cls])
+            print([str(obj) for obj in all_objs.values()
+                   if obj.__class__.__name__ == cls])
+
+    def help_all(self):
+        '''displays information about `all` command
+        '''
+        print("\nUsage-1: all"
+              "\nUsage-2: all <class-name>\n"
+              "-> prints string representation of all instances or based\n"
+              "   on their class-name.\n"
+              "\nDocumented class-names:\n"
+              "========================")
+        for cls in HBNBCommand.__classes:
+            print(cls)
+        print()
+
+    def do_update(self, line):
+        '''Updates an instance based on the class name and id
+        by adding or updating attributes.
+        '''
+        if not line:
+            print(HBNBCommand.__err_msgs["cls_missing"])
+            return
+
+        _cls = _id = _att_name = _att_val = all_objs = ''
+        line = shlex.split(line)
+        len_line = len(line)
+        if line[0] not in HBNBCommand.__classes:
+            print(HBNBCommand.__err_msgs["cls_unexist"])
+            return
+        else:
+            _cls = line[0]
+        
+        if len_line < 2:
+            print(HBNBCommand.__err_msgs["id_missing"])
+            return
+
+        _id = line[1]
+        all_objs = storage.all()
+        obj_key = f"{_cls}.{_id}"
+        if obj_key not in all_objs:
+            print(HBNBCommand.__err_msgs["id_unexist"])
+
+        if len_line < 3:
+            print(HBNBCommand.__err_msgs["attr_missing"])
+            return
+        if len_line < 4:
+            print(HBNBCommand.__err_msgs["val_missing"])
+            return
+        _att_name = line[2].replace(" ", "_")
+        _att_val = HBNBCommand.__cast_attribute(line[3])
+
+        if _att_name in ["id", "created_at", "updated_at"]:
+            print(f"`{_att_name}` value cannot be changed.")
+            return
+        setattr(all_objs[obj_key], _att_name, _att_val)
+        all_objs[obj_key].save()
+
+    def help_update(self):
+        '''displays information about the `update` command
+        '''
+        print("\nUsage: update <class-name> <id> <attribute-name> "
+              "\"<attribute value>\"\n"
+              "-> updates an instance/model based on the class-name and id\n"
+              "   by adding or updating an attribute.\n"
+              "-> Only one attribute can be updated at a time.\n"
+              "\nDocumented class-names:\n"
+              "========================")
+        for cls in HBNBCommand.__classes:
+            print(cls)
+        print()
+
 
     def do_destroy(self, line):
         '''Deletes an instance based on the class name and id
         '''
-        all_dict = storage.all()
+        all_objs = storage.all()
 
         if not line:
             print(HBNBCommand.__err_msgs["cls_missing"])
@@ -101,20 +210,24 @@ class HBNBCommand(cmd.Cmd):
             print(HBNBCommand.__err_msgs["cls_unexist"])
         elif len(line) < 2:
             print(HBNBCommand.__err_msgs["id_missing"])
-        elif f"{line[0]}.{line[1]}" not in all_dict:
+        elif f"{line[0]}.{line[1]}" not in all_objs:
             print(HBNBCommand.__err_msgs["id_unexist"])
-        elif all_dict.get(f"{line[0]}.{line[1]}", None):
-            del all_dict[f"{line[0]}.{line[1]}"]
+        elif all_objs.get(f"{line[0]}.{line[1]}", None):
+            del all_objs[f"{line[0]}.{line[1]}"]
             storage.save()
         else:
             print(HBNBCommand.__err_msgs["id_unexist"])
-            
-    def help_show(self):
-        print("\n".join(["Usage: show <class_name> <id>",
-                        "prints the string representation of an instance\
-                        based on the class-name and id"
-                         ]
-                        ))
+
+    def help_destroy(self):
+        '''displays information about the `destroy` command
+        '''
+        print("\nUsage: destroy <class-name> <id>\n"
+              "-> deletes an instance/model based on the class-name and id.\n"
+              "\nDocumented class-names:\n"
+              "========================")
+        for cls in HBNBCommand.__classes:
+            print(cls)
+        print()
 
 
 if __name__ == '__main__':
