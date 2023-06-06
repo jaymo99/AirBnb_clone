398dbf7867cad29df9a3d9bd810e94f4be6da9d0
James Kariuki <karisjaymo99@gmail.com>
2023-06-06 20:56:20 +0300

    add new classes to file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,7 +32,12 @@ class FileStorage():
 
     def reload(self):
         '''deserializes a JSON file to self.__objects'''
+        from models.amenity import Amenity
         from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
         from models.user import User
 
         try:
